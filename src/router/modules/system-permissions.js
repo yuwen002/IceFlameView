1b83906fce474a8b073b59dc1092bffc28f9b346
yuwen002 <yuwen002@163.com>
2023-04-24 23:54:58 +0800

    管理用户理表测试

--- src/router/modules/system-permissions.js
+++ src/router/modules/system-permissions.js
@@ -1,6 +1,7 @@
 const Layout = () => import('@/layout/index.vue')
 
-const SystemMasterList = () => import("@/views/system-permissions/show_system_master.vue");
+const SystemMasterList = () =>
+  import('@/views/system-permissions/show_system_master.vue')
 
 export default [
   {
@@ -22,43 +23,51 @@ export default [
         },
       },
       {
-        path: "",
-        name: "管理员角色绑定列表",
+        path: '',
+        name: '管理员角色绑定列表',
         component: '',
         meta: {
-          title: "管理员角色绑定列表",
+          title: '管理员角色绑定列表',
         },
       },
       {
-        path: "",
-        name: "权限列表",
+        path: '',
+        name: '权限列表',
         component: '',
         meta: {
-          title: "权限列表",
+          title: '权限列表',
         },
       },
       {
-        path: "/master/list",
-        name: "管理员用户列表",
+        path: '/master/list',
+        name: '管理员用户列表',
         component: SystemMasterList,
         meta: {
-          title: "管理员用户列表",
+          title: '管理员用户列表',
         },
       },
       {
-        path: "",
-        name: "访问类型列表",
-        component: '',
+        path: '/master/add',
+        name: '新增管理员用户',
+        component: SystemMasterList,
         meta: {
-          title: "访问类型列表",
+          title: '新增管理员用户',
         },
       },
       {
-        path: "",
-        name: "访问日志列表",
+        path: '',
+        name: '访问类型列表',
         component: '',
         meta: {
-          title: "访问日志列表",
+          title: '访问类型列表',
+        },
+      },
+      {
+        path: '',
+        name: '访问日志列表',
+        component: '',
+        meta: {
+          title: '访问日志列表',
         },
       },
     ],
