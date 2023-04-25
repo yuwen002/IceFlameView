87f18795ebc0006632cac2fa1c07d313780f73ca
yuwen002 <yuwen002@163.com>
2023-04-26 01:40:23 +0800

    用户管理研究

--- src/router/modules/system-permissions.js
+++ src/router/modules/system-permissions.js
@@ -2,6 +2,8 @@ const Layout = () => import('@/layout/index.vue')
 
 const SystemMasterList = () =>
   import('@/views/system-permissions/show_system_master.vue')
+const SystemMasterAdd = () =>
+  import('@/views/system-permissions/add_system_master.vue')
 
 export default [
   {
@@ -14,31 +16,6 @@ export default [
     icon: 'icon-home',
     children: [
       {
-        path: '',
-        name: '管理员角色列表',
-        component: '',
-        meta: {
-          title: '管理员角色列表',
-          affix: true,
-        },
-      },
-      {
-        path: '',
-        name: '管理员角色绑定列表',
-        component: '',
-        meta: {
-          title: '管理员角色绑定列表',
-        },
-      },
-      {
-        path: '',
-        name: '权限列表',
-        component: '',
-        meta: {
-          title: '权限列表',
-        },
-      },
-      {
         path: '/master/list',
         name: '管理员用户列表',
         component: SystemMasterList,
@@ -47,29 +24,63 @@ export default [
         },
       },
       {
-        path: '/master/add',
-        name: '新增管理员用户',
+        path: '/master/list',
+        name: '新建管理员用户',
         component: SystemMasterList,
         meta: {
-          title: '新增管理员用户',
+          title: '新建管理员用户',
         },
+        hidden: true,
       },
-      {
-        path: '',
-        name: '访问类型列表',
-        component: '',
-        meta: {
-          title: '访问类型列表',
-        },
-      },
-      {
-        path: '',
-        name: '访问日志列表',
-        component: '',
-        meta: {
-          title: '访问日志列表',
-        },
-      },
+      // {
+      //   path: '',
+      //   name: '管理员角色列表',
+      //   component: '',
+      //   meta: {
+      //     title: '管理员角色列表',
+      //     affix: true,
+      //   },
+      // },
+      // {
+      //   path: '',
+      //   name: '管理员角色绑定列表',
+      //   component: '',
+      //   meta: {
+      //     title: '管理员角色绑定列表',
+      //   },
+      // },
+      // {
+      //   path: '',
+      //   name: '权限列表',
+      //   component: '',
+      //   meta: {
+      //     title: '权限列表',
+      //   },
+      // },
+      // {
+      //   path: '/master/add',
+      //   name: '添加管理员用户',
+      //   component: SystemMasterAdd,
+      //   meta: {
+      //     title: '添加管理员用户',
+      //   },
+      // },
+      // {
+      //   path: '',
+      //   name: '访问类型列表',
+      //   component: '',
+      //   meta: {
+      //     title: '访问类型列表',
+      //   },
+      // },
+      // {
+      //   path: '',
+      //   name: '访问日志列表',
+      //   component: '',
+      //   meta: {
+      //     title: '访问日志列表',
+      //   },
+      // },
     ],
   },
 ]
---
+++ src/views/system-permissions/add_system_master.vue
@@ -0,0 +1,11 @@
+<template>
+  bbbbbbbbbbb
+</template>
+
+<script>
+export default {
+  name: 'add_system_master',
+}
+</script>
+
+<style scoped></style>
