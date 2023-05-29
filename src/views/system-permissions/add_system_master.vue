b4e08901301d525f7a2dfb4d298eaa12b8c68664
yuwen002 <yuwen002@163.com>
2023-05-29 14:22:57 +0800

    新增新建管理员

--- src/views/system-permissions/add_system_master.vue
+++ src/views/system-permissions/add_system_master.vue
@@ -37,90 +37,96 @@ import {
   toRefs,
   ref,
   computed,
-  watch
-} from "vue";
-import { AddSystemMaster } from "@/api/system-permissions";
+  watch,
+} from 'vue'
+import { AddSystemMaster } from '@/api/system-permissions'
 
 export default defineComponent({
-  name: "systemMasterAdd",
+  name: 'systemMasterAdd',
   setup() {
-    const { proxy: ctx } = getCurrentInstance(); // 可以把ctx当成vue2中的this
-    watch(() => state.addForm, (addForm) => {
-      if (addForm) {
-        state.addForm.value = addForm.model
-      }
-    })
+    const { proxy: ctx } = getCurrentInstance() // 可以把ctx当成vue2中的this
+    // watch(() => state.addForm, (addForm) => {
+    //   console.log(addForm.model, state.addForm.value)
+    //   if (addForm) {
+    //     state.addForm.value = addForm.model
+    //   }
+    // })
     const getRules = () => ({
       username: [
-        { required: true, message: "请输入用户名", trigger: "blur" },
-        { min: 5, max: 32, message: "长度在 5 到 32 个字符", trigger: "blur" }
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+        { min: 5, max: 32, message: '长度在 5 到 32 个字符', trigger: 'blur' },
       ],
       tel: [
-        { required: true, message: "手机号码不能为空", trigger: ["blur", "change"] },
-        { pattern: /^1[3456789]\d{9}$/, message: "手机号码格式不正确", trigger: "blur" }
+        {
+          required: true,
+          message: '手机号码不能为空',
+          trigger: ['blur', 'change'],
+        },
+        {
+          pattern: /^1[3456789]\d{9}$/,
+          message: '手机号码格式不正确',
+          trigger: 'blur',
+        },
       ],
       password: [
-        { required: true, message: "请输入密码", trigger: "blur" },
-        { min: 5, max: 32, message: "长度在 5 到 32 个字符", trigger: "blur" }
+        { required: true, message: '请输入密码', trigger: 'blur' },
+        { min: 5, max: 32, message: '长度在 5 到 32 个字符', trigger: 'blur' },
       ],
       confirmPassword: [
-        { required: true, message: "请再次输入密码", trigger: "blur" },
+        { required: true, message: '请再次输入密码', trigger: 'blur' },
         {
           validator: (rule, value) => {
-            if (value === "") {
-              return Promise.reject("请再次输入密码");
-            } else if (value !== state.addForm.value.password) {
-              return Promise.reject("两次输入密码不一致");
+            if (value === '') {
+              return Promise.reject('请再次输入密码')
+            } else if (value !== state.model.password) {
+              return Promise.reject('两次输入密码不一致')
             } else {
-              return Promise.resolve();
+              return Promise.resolve()
             }
           },
-          trigger: "blur"
-        }
+          trigger: 'blur',
+        },
       ],
-      name: [
-        { required: true, message: "请输入姓名", trigger: "blur" }
-      ]
-    });
+      name: [{ required: true, message: '请输入姓名', trigger: 'blur' }],
+    })
     const state = reactive({
       model: {
-        username: "",
-        tel: "",
-        password: "",
-        confirmPassword: "",
-        name: ""
+        username: '',
+        tel: '',
+        password: '',
+        confirmPassword: '',
+        name: '',
       },
       rules: getRules(),
       loading: false,
-      btnText: computed(() =>
-        state.loading ? "提交中..." : "提交"
-      ),
+      btnText: computed(() => (state.loading ? '提交中...' : '提交')),
       addForm: ref(null),
       submit: () => {
         if (state.loading) {
-          return;
+          return
         }
+
         state.addForm.validate(async valid => {
           if (valid) {
-            state.loading = true;
-            const { code, message } = await AddSystemMaster(state.model);
+            state.loading = true
+            const { code, message } = await AddSystemMaster(state.model)
             if (+code === 0) {
               ctx.$message.success({
                 message: message,
-                duration: 1000
-              });
+                duration: 1000,
+              })
             } else {
-              ctx.$message.error(message);
+              ctx.$message.error(message)
             }
-            state.loading = false;
+            state.loading = false
           }
-        });
-      }
-    });
+        })
+      },
+    })
 
     return {
-      ...toRefs(state)
-    };
-  }
-});
+      ...toRefs(state),
+    }
+  },
+})
 </script>
