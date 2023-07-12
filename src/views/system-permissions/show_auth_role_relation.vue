dc19d57366976e1d335724a70206735c9c602847
yuwen002 <yuwen002@163.com>
2023-07-12 18:08:37 +0800

    新增角色权限绑定修改

--- src/views/system-permissions/show_auth_role_relation.vue
+++ src/views/system-permissions/show_auth_role_relation.vue
@@ -8,21 +8,45 @@
   >
     <!-- 工具栏 -->
     <template #toolbar>
-      <el-button type="primary" @click="$router.push({ name: 'authRoleRelationAdd' })">
-        <el-icon><Plus /></el-icon>&nbsp;添加权限绑定
+      <el-button
+        type="primary"
+        @click="$router.push({ name: 'authRoleRelationAdd' })"
+      >
+        <el-icon>
+          <Plus />
+        </el-icon>
+        &nbsp;添加权限绑定
       </el-button>
-      <el-button type="danger" @click="refresh"><el-icon><Refresh /></el-icon>&nbsp;刷新</el-button>
+      <el-button type="danger" @click="refresh">
+        <el-icon>
+          <Refresh />
+        </el-icon>
+        &nbsp;刷新
+      </el-button>
     </template>
 
-    <template #account_id_and_name="text"> <!-- 注意这里使用了 v-slot 指令 -->
+    <template #account_id_and_name="text">
+      <!-- 注意这里使用了 v-slot 指令 -->
       <span>{{ `(${text.row.account_id})${text.row.name}` }}</span>
     </template>
 
     <!-- 操作列 -->
     <template #operate="scope">
-      <el-button size="mini" type="primary" @click="$router.push({ name: 'authRoleRelationShowEdit', query: { role_id: scope.row.role_id } })">权限分配</el-button>
-      <el-button size="mini" type="primary" @click="handleEdit(scope.row)">编辑</el-button>
-      <el-button size="mini" :type="scope.row.status === 1 ? 'warning' : 'danger'" @click="handleStatus(scope.row)">{{ scope.row.status === 1 ? '启用' : '停用' }}</el-button>
+      <el-button
+        size="mini"
+        type="primary"
+        @click="
+          $router.push({
+            name: 'authRoleRelationShowEdit',
+            query: { role_id: scope.row.role_id },
+          })
+        "
+      >
+        权限分配
+      </el-button>
+      <el-button size="mini" type="primary" @click="handleEdit(scope.row)">
+        编辑
+      </el-button>
     </template>
   </pro-table>
 
@@ -42,7 +66,10 @@
         </el-select>
       </el-form-item>
       <el-form-item label="管理员信息">
-        <el-select v-model="currentData.account_id" placeholder="请选择决管理员信息">
+        <el-select
+          v-model="currentData.account_id"
+          placeholder="请选择决管理员信息"
+        >
           <el-option
             v-for="item in accountIdOptions"
             :key="item.account_id"
@@ -57,21 +84,20 @@
       <el-button type="primary" @click="handleSubmit">确 定</el-button>
       <el-button @click="dialogVisible = false">取 消</el-button>
     </template>
-
   </el-dialog>
 </template>
 
 <script>
-import { getCurrentInstance, onMounted, reactive, ref, toRefs } from "vue";
+import { getCurrentInstance, onMounted, reactive, ref, toRefs } from 'vue'
 import {
   EditAuthRoleRelation,
   GetAllSystemMaster,
   GetAuthRole,
-  ShowAuthRoleRelation
-} from "@/api/system-permissions";
+  ShowAuthRoleRelation,
+} from '@/api/system-permissions'
 
 export default {
-  name: "authRoleRelationList",
+  name: 'authRoleRelationList',
   setup() {
     const { proxy: ctx } = getCurrentInstance()
     const proTable = ref(null)
@@ -83,39 +109,39 @@ export default {
 
     const state = reactive({
       columns: [
-        { label: "序号", type: "index" },
-        { label: "管理员ID", prop: "account_id" },
-        { label: "管理员名称",prop: "name" },
-        { label: "角色ID", prop: "role_id" },
-        { label: "角色名称", prop: "role_name" },
-        { label: "创建时间", prop: "created_at" },
-        { label: "修改时间", prop: "updated_at" },
+        { label: '序号', type: 'index' },
+        { label: '管理员ID', prop: 'account_id' },
+        { label: '管理员名称', prop: 'name' },
+        { label: '角色ID', prop: 'role_id' },
+        { label: '角色名称', prop: 'role_name' },
+        { label: '创建时间', prop: 'created_at' },
+        { label: '修改时间', prop: 'updated_at' },
         {
-          label: "操作",
-          fixed: "right",
-          width: 280,
-          align: "center",
-          tdSlot: "operate" // 自定义单元格内容的插槽名称
-        }
+          label: '操作',
+          fixed: 'right',
+          width: 200,
+          align: 'center',
+          tdSlot: 'operate', // 自定义单元格内容的插槽名称
+        },
       ],
       dialogVisible: false,
       currentData: ref(null),
       paginationConfig: {
-        layout: "total, prev, pager, next, sizes",
+        layout: 'total, prev, pager, next, sizes',
         pageSize: 10,
         pageSizes: [10, 20, 50, 100],
       },
     })
 
-    const getList = async (params) => {
+    const getList = async params => {
       const { code, data, message } = await ShowAuthRoleRelation({
         page: params.current,
-        size: params.size
+        size: params.size,
       })
       // 必须要返回一个对象，包含data数组和total总数
       return {
         data: data.list,
-        total: +data.total
+        total: +data.total,
       }
     }
 
@@ -146,9 +172,9 @@ export default {
       fetchAccountIdOptions()
     })
 
-    const handleEdit = (row) => {
+    const handleEdit = row => {
       console.log(row)
-      state.currentData = { ...row}
+      state.currentData = { ...row }
       state.dialogVisible = true
     }
 
@@ -157,7 +183,7 @@ export default {
         // 获取表单数据
         const formData = { ...state.currentData }
         // console.log(formData)
-        const { code, message } =  await EditAuthRoleRelation(formData)
+        const { code, message } = await EditAuthRoleRelation(formData)
 
         if (+code === 0) {
           ctx.$message.success({
@@ -175,6 +201,7 @@ export default {
       }
     }
 
+
     return {
       ...toRefs(state),
       proTable,
@@ -185,10 +212,8 @@ export default {
       handleEdit,
       handleSubmit,
     }
-  }
+  },
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
