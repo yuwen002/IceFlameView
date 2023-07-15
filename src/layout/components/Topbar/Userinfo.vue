0374fa8c510a39bf711d10db8f5bc377df9f7ec1
yuwen002 <yuwen002@163.com>
2023-07-15 18:31:03 +0800

    增加密码修改功能

--- src/layout/components/Topbar/Userinfo.vue
+++ src/layout/components/Topbar/Userinfo.vue
@@ -17,24 +17,24 @@
  *          \  \ `_.   \_ __\ /__ _/   .-` /  /
  *      =====`-.____`.___ \_____/___.-`___.-'=====
  *                        `=---='
- * 
- * 
+ *
+ *
  *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * 
+ *
  *            佛祖保佑       永不宕机     永无BUG
- * 
- *        佛曰:  
- *                写字楼里写字间，写字间里程序员；  
- *                程序人员写程序，又拿程序换酒钱。  
- *                酒醒只在网上坐，酒醉还来网下眠；  
- *                酒醉酒醒日复日，网上网下年复年。  
- *                但愿老死电脑间，不愿鞠躬老板前；  
- *                奔驰宝马贵者趣，公交自行程序员。  
- *                别人笑我忒疯癫，我笑自己命太贱；  
+ *
+ *        佛曰:
+ *                写字楼里写字间，写字间里程序员；
+ *                程序人员写程序，又拿程序换酒钱。
+ *                酒醒只在网上坐，酒醉还来网下眠；
+ *                酒醉酒醒日复日，网上网下年复年。
+ *                但愿老死电脑间，不愿鞠躬老板前；
+ *                奔驰宝马贵者趣，公交自行程序员。
+ *                别人笑我忒疯癫，我笑自己命太贱；
  *                不见满街漂亮妹，哪个归得程序员？
- * 
- * @Descripttion: 
- * @version: 
+ *
+ * @Descripttion:
+ * @version:
  * @Date: 2021-04-20 11:06:21
  * @LastEditors: huzhushan@126.com
  * @LastEditTime: 2022-09-27 17:56:21
@@ -59,7 +59,7 @@
     <template #dropdown>
       <el-dropdown-menu>
         <el-dropdown-item>{{ $t('topbar.center') }}</el-dropdown-item>
-        <el-dropdown-item>{{ $t('topbar.password') }}</el-dropdown-item>
+        <el-dropdown-item @click="modifyPassword">{{ $t('topbar.password') }}</el-dropdown-item>
         <lock-modal />
         <el-dropdown-item @click="logout">
           {{ $t('topbar.logout') }}
@@ -91,9 +91,14 @@ export default defineComponent({
       router.push('/login')
     }
 
+    const modifyPassword = () => {
+      router.push({name: ''})
+    }
+
     return {
       userinfo,
       logout,
+      modifyPassword,
     }
   },
 })
---
+++ src/views/home/modify_password.vue
@@ -0,0 +1,92 @@
+<template>
+  <el-form :model="model" :rules="rules" ref="addForm" label-width="100px">
+    <el-form-item label="旧密码" prop="old_password">
+      <el-input v-model.trim="model.old_password"></el-input>
+    </el-form-item>
+    <el-form-item label="新密码" prop="new_password">
+      <el-input v-model.trim="model.new_password"></el-input>
+    </el-form-item>
+    <el-form-item label="确认新密码" prop="confirm_new_password">
+      <el-input v-model.trim="model.confirm_new_password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button
+        :loading="loading"
+        type="primary"
+        size="large"
+        @click="submit"
+      >
+        {{ btnText }}
+      </el-button>
+
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+import { computed, getCurrentInstance, ref } from "vue";
+import { AddAuthRole } from "@/api/system-permissions";
+
+export default {
+  name: "modifyPassword",
+  setup() {
+    let loading = false
+
+    const { proxy: ctx } = getCurrentInstance()
+    const btnText = computed(() => (loading ? '提交中...' : '提交'))
+    const addForm = ref(null)
+
+    const rules = {
+      name: [
+        {
+          required: true,
+          message: '角色名称不能为空',
+          trigger: ['blur', 'change'],
+        },
+      ],
+    }
+
+    const model = ref({
+      old_password: '',
+      new_password: '',
+      confirm_new_password: '',
+    })
+
+    const submit = () => {
+      if (loading) {
+        return
+      }
+
+      addForm.value.validate(async (valid) => {
+        if (valid) {
+          loading = true
+          const { code, message } = await AddAuthRole(model.value)
+          if (+code === 0) {
+            ctx.$message.success({
+              message: message,
+              duration: 1000,
+            })
+            addForm.value.resetFields()
+          } else {
+            ctx.$message.error(message)
+          }
+          loading = false
+        }
+      })
+    }
+
+    return {
+      loading,
+      btnText,
+      addForm,
+      rules,
+      model,
+      submit,
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
